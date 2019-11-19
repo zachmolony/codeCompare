b7ca5d587d7160735aa6c7b1ba6052c1b9c2c961
zach m <zachary.molony@ada.ac.uk>
2019-11-19 12:10:49 +0000

    added search content

--- components/Search.vue
+++ components/Search.vue
@@ -1,38 +1,53 @@
 <template>
   <div class="box">
     <article class="media">
-        <div class="media-left">
-        <figure class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
-        </figure>
-        </div>
         <div class="media-content">
-        <div class="content">
-            <p>
-            <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-            <br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-            </p>
-        </div>
-        <nav class="level is-mobile">
-            <div class="level-left">
-            <a class="level-item" aria-label="reply">
-                <span class="icon is-small">
-                <i class="fas fa-reply" aria-hidden="true"></i>
-                </span>
-            </a>
-            <a class="level-item" aria-label="retweet">
-                <span class="icon is-small">
-                <i class="fas fa-retweet" aria-hidden="true"></i>
-                </span>
-            </a>
-            <a class="level-item" aria-label="like">
-                <span class="icon is-small">
-                <i class="fas fa-heart" aria-hidden="true"></i>
-                </span>
-            </a>
+            <h1 class="subtitle is-size-3">Filter Results</h1>
+            <div class="content">
+                <strong>Language</strong><br>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
             </div>
-        </nav>
+            <div class="content">
+                <strong>Proficiency</strong><br>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+            </div>
+            <div class="content">
+                <strong>Learning Style</strong><br>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Kinestetic
+                </label>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+                <label class="checkbox">
+                    <input type="checkbox">
+                    Remember me
+                </label>
+            </div>
         </div>
     </article>
     </div>
@@ -44,6 +59,7 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="sass" scoped>
+.content
+    margin-top: -20px !important
 </style>
