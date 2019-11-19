73b53c8614df39bf947db6f74bae0c0681ce9e8f
zach m <zachary.molony@ada.ac.uk>
2019-11-19 11:47:49 +0000

    created components

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,49 +1,52 @@
 <template>
     <div>
-        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <nav class="navbar hero is-primary is-bold" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                </a>
+                <nuxt-link to="/" class="navbar-item">
+                    <img class="img" src="../assets/logo.png" height="28">
+                    <title class="has-text-sucess">CodeCompare</title>
+                </nuxt-link>
 
                 <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 <span aria-hidden="true"></span>
                 </a>
-            </div>
+            
 
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                <a class="navbar-item">
-                    Home
-                </a>
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                    <nuxt-link to="/" class="navbar-item">
+                        Home
+                    </nuxt-link>
 
-                <a class="navbar-item">
-                    Compare Resources
-                </a>
+                    <nuxt-link to="/" class="navbar-item">
+                        Compare
+                    </nuxt-link>
 
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                    More
-                    </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            More
+                        </a>
 
-                    <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                        About
-                    </a>
-                    <a class="navbar-item">
-                        Jobs
-                    </a>
-                    <a class="navbar-item">
-                        Contact
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
+                        <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            About
+                        </a>
+                        <a class="navbar-item">
+                            Jobs
+                        </a>
+                        <a class="navbar-item">
+                            Contact
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Report an issue
+                        </a>
+                        </div>
                     </div>
-                </div>
+                    </div>
+
                 </div>
 
                 <div class="navbar-end">
@@ -74,6 +77,15 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="sass" scoped>
+.navbar
+    height: 10vh
+    background-color: grey !important
+
+.navbar
+    padding-top: 1.5vh
+
+.img
+    max-width: 100% !important
 
 </style>
