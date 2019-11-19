ef6a3c80747f1530a7f8e33fe5d0a5e005288e66
zach m <zachary.molony@ada.ac.uk>
2019-11-19 15:33:57 +0000

    created component for results

---
+++ components/Results.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="results">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+    </div>
+</template>
+
+<script>
+import Card from '~/components/Card.vue'
+
+export default {
+    name: 'Results',
+    components: {
+        Card
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+.results
+  display: flex
+  flex-wrap: wrap
+</style>
