21af14db569b58efb9138e53e0fd4b18c8ed986e
zach m <zachary.molony@ada.ac.uk>
2019-11-19 11:48:50 +0000

    added more to home content

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,52 @@
 <template>
-  <div class="container is-primary is-bold">
-    <div>
-      <logo />
-      <h1 class="title has-text-success">
-        CodeCompare
-      </h1>
-      <h2 class="subtitle">
-        The No.1 Destination for Coding Resources
-      </h2>
-      <div class="links">
-        <nuxt-link to="/compare" class="button--green">
-          Compare Resources
-        </nuxt-link>
-        <nuxt-link to="/compare" class="button--grey">
-          Sign-up
-        </nuxt-link>
+  <div>
+    <div class="container hero is-primary is-bold">
+      <div class="hero-content">
+        <logo />
+        <h1 class="title has-text-grey">
+          CodeCompare
+        </h1>
+        <h2 class="subtitle">
+          The No.1 Destination for Coding Resources
+        </h2>
+        <div class="links">
+          <nuxt-link to="/compare" class="button--green">
+            Compare Resources
+          </nuxt-link>
+          <nuxt-link to="/compare" class="button--grey">
+            Sign-up
+          </nuxt-link>
+        </div>
       </div>
     </div>
+
+    <div class="info">
+      <div class="columns">
+        <div class="column">
+          First column
+        </div>
+        <div class="column">
+          Second column
+        </div>
+        <div class="column">
+          Third column
+        </div>
+        <div class="column">
+          Fourth column
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong class="has-text-success">CodeCompare</strong> by <a href="https://github.com/zachmolony">Team 000D</a>. © 2019
+        </p>
+      </div>
+    </footer>
+
   </div>
+  
 </template>
 
 <script>
@@ -34,10 +63,15 @@ export default {
 .container {
   margin: 0 auto;
   min-height: 100vh;
+  min-width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+}
+
+.hero-content {
+  margin-top: -10vh;
 }
 
 .title {
@@ -60,5 +94,20 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.info {
+  min-width: 100vw;
+  min-height: 60vh;
+}
+
+footer {
+  color: white;
+  background-color: #526488 !important;
+}
+
+a {
+  color: white;
+  text-decoration-line: underline;
 }
 </style>
