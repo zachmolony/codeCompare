d711197b11c9890f6261046cdbadbf362dfd760b
zach m <zachary.molony@ada.ac.uk>
2019-11-19 15:33:42 +0000

    remade card component

--- components/Card.vue
+++ components/Card.vue
@@ -1,35 +1,31 @@
 <template>
   <div>
-      <div class="card">
-        <div class="card-image">
-            <figure class="image">
-                <img src="../assets/cc-logo.jpeg" class="card-img">
-            </figure>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image">
+          <img src="../assets/cc-logo.jpeg" class="card-img">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">Javascript</p>
+            <p class="subtitle is-6"><a href="https://codecademy.com">codecademy.com</a></p>
+          </div>
         </div>
-        <div class="card-content">
-            <div class="media">
-            <div class="media-content">
-                <p class="title is-4">Javascript</p>
-                <p class="subtitle is-6"><a href="https://codecademy.com">codecademy.com</a></p>
-            </div>
-            </div>
 
-            <div class="ccontent">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Phasellus nec iaculis mauris. <br>
-            <div class="rating">
-                <figure class="image is-32x32">
-                    <img src="../assets/thumbs-up.png" alt="">
-                </figure>
-                <figure class="image is-32x32">
-                    <img src="../assets/thumbs-down.png" alt="">
-                </figure>
-                <p>+30</p>
-            </div>
-            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
+        <div class="ccontent">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <br>
+          <div class="rating">
+            <img src="../assets/like.png" alt="">
+            <p class="has-text-weight-bold is-size-5 has-text-success ">+30</p>
+            <img src="../assets/dislike.png" alt="">
+          </div>
+          <time class="is-size-7" datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
-        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,6 +36,10 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.card
+    max-width: 15vw
+    margin: 1vw
+
 a 
     color: grey
 
@@ -48,12 +48,17 @@ a
 
 .card-img
     padding-top: 2em
+    max-width: 20vw
 
 .rating
+    padding-top: 20px
     width: 100%
     display: flex
     justify-content: space-between
     align-self: center
+    vertical-align: middle
 
-
+.rating > img
+    width: auto
+    height: 30px
 </style>
