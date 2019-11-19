f50715de82d539c1ba0576ffe1e2abde7758e5bd
zach m <zachary.molony@ada.ac.uk>
2019-11-19 15:34:21 +0000

    made it prettier

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,69 +1,70 @@
 <template>
-    <div>
-        <nav class="navbar hero is-primary is-bold" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <nuxt-link to="/" class="navbar-item">
-                    <img class="img" src="../assets/logo.png" height="28">
-                    <title class="has-text-sucess">CodeCompare</title>
-                </nuxt-link>
+  <div>
+    <nav class="navbar hero is-primary is-bold" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">
+          <img class="img" src="../assets/logo.png" height="28">
+          <h1 class="title is-size-4">CodeCompare</h1>
+        </nuxt-link>
 
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+          data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <nuxt-link to="/" class="navbar-item">
+              Home
+            </nuxt-link>
+
+            <nuxt-link to="/" class="navbar-item">
+              Compare
+            </nuxt-link>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
                 </a>
-            
-
-                <div id="navbarBasicExample" class="navbar-menu">
-                    <div class="navbar-start">
-                    <nuxt-link to="/" class="navbar-item">
-                        Home
-                    </nuxt-link>
-
-                    <nuxt-link to="/" class="navbar-item">
-                        Compare
-                    </nuxt-link>
-
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
-                            More
-                        </a>
-
-                        <div class="navbar-dropdown">
-                        <a class="navbar-item">
-                            About
-                        </a>
-                        <a class="navbar-item">
-                            Jobs
-                        </a>
-                        <a class="navbar-item">
-                            Contact
-                        </a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item">
-                            Report an issue
-                        </a>
-                        </div>
-                    </div>
-                    </div>
-
-                </div>
-
-                <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                    </div>
-                </div>
-                </div>
+                <a class="navbar-item">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
             </div>
-            </nav>
-    </div>
+          </div>
+
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -87,5 +88,9 @@ export default {
 
 .img
     max-width: 100% !important
+    padding-right: 20px
+
+h1
+    text-decoration: none !important
 
 </style>
